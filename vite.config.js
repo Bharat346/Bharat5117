d7c6bcf11f3910e880b82e0aed4850f1fee16694
Bharat kumar <bharat030406@gmail.com>
2024-07-15 06:45:33 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/Bharat5117",
   plugins: [
     react(),
     nodePolyfills()
